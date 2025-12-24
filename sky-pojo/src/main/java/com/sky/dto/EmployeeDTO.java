52aef284c7a77ab951d9c818dff406989aa9632f
nansitian <1652109926@qq.com>
2025-12-24 11:47:37 +0800

    新增员工业务代码开发

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,37 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-public class EmployeeDTO implements Serializable {
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ApiModel(description = "新增员工时传递的数据模型")
+public class EmployeeDTO implements Serializable {//1.获得化作流的资格, 可以存如redis  2.对象被写到硬盘里面持久化 3.会有版本锁定功能"版本指纹"
 
+    @ApiModelProperty("主键ID")
     private Long id;
 
+    @ApiModelProperty("账号")
     private String username;
 
+    @ApiModelProperty("员工姓名")
     private String name;
 
+    @ApiModelProperty("手机号")
     private String phone;
 
+    @ApiModelProperty("性别")
     private String sex;
 
+    @ApiModelProperty("身份证号")
     private String idNumber;
 
 }
--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -19,9 +22,28 @@ public class GlobalExceptionHandler {
      * @return
      */
     @ExceptionHandler
-    public Result exceptionHandler(BaseException ex){
+    public Result exceptionHandler(BaseException ex){//这个异常是自定义的异常, 所有自己抛出的异常都会别这个异常捕获(系统自己的异常无法捕获)
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    /**
+     * 处理sql异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //Duplicate entry 'zhangsan' for key 'employee.idx_username'
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String[] split = message.split(" ");
+            String username = split[2];
+            String errorMessage = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(errorMessage);
+        }else {
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
     }
 
 }
