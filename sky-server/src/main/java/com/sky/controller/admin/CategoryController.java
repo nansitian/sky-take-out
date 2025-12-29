00c6ccd53128c200e28c84137dc77fe9f12d6ed7
nansitian <1652109926@qq.com>
2025-12-29 16:18:32 +0800

    编辑分类业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/CategoryController.java
@@ -0,0 +1,103 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.annotations.Delete;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequestMapping("/admin/category")
+@Api(tags = "分类相关接口")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    /**
+     * 修改分类
+     * @param categoryDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改分类")
+    public Result update(@RequestBody CategoryDTO categoryDTO){
+        log.info("修改分类: {}",categoryDTO);
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+
+    /**
+     * 新增分类
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增分类")
+    public Result save(@RequestBody CategoryDTO categoryDTO){
+        log.info("新增分类: {}",categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分类分页查询")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO){
+        log.info("分类分页查询: {}",categoryPageQueryDTO);
+        PageResult pageResult = categoryService.page(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 启用、禁用分类
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用、禁用分类")
+    public Result startOrStop(@PathVariable Integer status, Long id){
+        log.info("启用、禁用分类: {} {}",status,id);
+        categoryService.startOrStop(status,id);
+        return Result.success();
+    }
+
+    @DeleteMapping
+    @ApiOperation("根据id删除分类")
+    public Result deleteById(Long id){
+        log.info("根据id删除分类: {}", id);
+        categoryService.deleteById(id);
+        return Result.success();
+    }
+
+    @GetMapping("/list")
+    @ApiOperation("根据类型查询分类")
+    public Result<List<Category>> selectByType(Integer type){
+        log.info("根据类型查询分类: {}",type);
+        List<Category> list = categoryService.selectByType(type);
+        return Result.success(list);
+    }
+
+
+
+
+
+
+
+
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,126 @@
+package com.sky.service.impl;
+
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.sky.context.BaseContext;
+import com.sky.dto.CategoryDTO;
+import com.sky.dto.CategoryPageQueryDTO;
+import com.sky.entity.Category;
+import com.sky.mapper.CategoryMapper;
+import com.sky.result.PageResult;
+import com.sky.service.CategoryService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+    @Autowired
+    CategoryMapper categoryMapper;
+
+    /**
+     * 修改分类
+     * @param categoryDTO
+     */
+    @Override
+    public void update(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        //对象属性拷贝
+        BeanUtils.copyProperties(categoryDTO,category);
+        //设置跟新时间
+        category.setUpdateTime(LocalDateTime.now());
+        //设置跟新人id
+        //获取跟新人id
+        Long currentId = BaseContext.getCurrentId();
+        category.setUpdateUser(currentId);
+        categoryMapper.update(category);
+
+    }
+
+    /**
+     * 新增分类
+     * @param categoryDTO
+     */
+    @Override
+    public void save(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        //对象属性拷贝
+        BeanUtils.copyProperties(categoryDTO,category);
+        //设置跟新时间
+        category.setUpdateTime(LocalDateTime.now());
+        //设置创建时间
+        category.setCreateTime(LocalDateTime.now());
+        //设置跟新人id
+        //获取跟新人id
+        Long currentId = BaseContext.getCurrentId();
+        category.setUpdateUser(currentId);
+        category.setCreateUser(currentId);
+        //设置状态,默认为0(禁用)
+        category.setStatus(0);
+        categoryMapper.insert(category);
+    }
+
+    /**
+     * 分类分页查询
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @Override
+    public PageResult page(CategoryPageQueryDTO categoryPageQueryDTO) {
+        //开始分页查询
+        PageHelper.startPage(categoryPageQueryDTO.getPage(),categoryPageQueryDTO.getPageSize());
+        Page<Category> page = (Page<Category>) categoryMapper.select(categoryPageQueryDTO);
+        //获取总记录数
+        long total = page.getTotal();
+        //获取查询到的集合
+        List<Category> result = page.getResult();
+        //创建pageresult 传入参数
+        PageResult pageResult = new PageResult(total,result);
+        return pageResult;
+    }
+
+    /**
+     * 启用、禁用分类
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+        Category category = new Category();
+        category.setStatus(status);
+        category.setId(id);
+        //设置跟新时间
+        category.setUpdateTime(LocalDateTime.now());
+        //设置跟新人id
+        //获取跟新人id
+        Long currentId = BaseContext.getCurrentId();
+        category.setUpdateUser(currentId);
+        categoryMapper.update(category);
+
+    }
+
+    /**
+     * 根据id删除分类
+     * @param id
+     */
+    @Override
+    public void deleteById(Long id) {
+        categoryMapper.deleteById(id);
+    }
+
+    /**
+     * 根据类型查询分类
+     * @param type
+     * @return
+     */
+    @Override
+    public List<Category> selectByType(Integer type) {
+        CategoryPageQueryDTO categoryPageQueryDTO = new CategoryPageQueryDTO();
+        categoryPageQueryDTO.setType(type);
+        List<Category> category = categoryMapper.select(categoryPageQueryDTO);
+        return category;
+    }
+}
